bfeaa330bfd84675af17bb526a461f0d23a9d8cf
Tebarek Abas <tebarekabas4@gmail.com>
2024-09-25 22:07:58 +0300

    Add the button functionality

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newButton = document.getElementById('new-quote');
 
-let apiQuotes = [];
+// let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
@@ -12,37 +12,51 @@ function newQuote() {
     // pick random quote array local data //
 
     //********************************** */
-    // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    // quoteText.textContent = quote.text;
-    // authorText.textContent = quote.author;
-    // console.log(quote)
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+    console.log(quote)
 
 
     // ******************************/
     // pick random quote array API data 
     // ********************************/
 
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    authorText.textContent = quote.author;
-    quoteText.textContent = quote.text;
-    console.log(quote)
+    // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // authorText.textContent = quote.author;
+    // quoteText.textContent = quote.text;
+    // console.log(quote)
    
 }
+// if (quote.text.length > 120) {
+//     quoteText.classList.add('long-quote')
+// }
+function tweetQuote() {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+  }
+
 newQuote();
+
+
+//  Event listener
+newButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote);
+    
 
 // Quotes in API
 
-async function getQuotes() {
-    const apiURL = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiURL); 
-        apiQuotes = await response.json();
-        console.log(apiQuotes[12])
-    }catch(error){
-        console.log(`The error is found ${error}`)
-    }
-}
+// async function getQuotes() {
+//     const apiURL = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiURL); 
+//         apiQuotes = await response.json();
+//         console.log(apiQuotes[12])
+//     }catch(error){
+//         console.log(`The error is found ${error}`)
+//     }
+// }
 
 //  on Load
-getQuotes();
+// getQuotes();
 
